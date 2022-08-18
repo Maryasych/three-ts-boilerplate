95196bd4bf5f47c6e9292aad5ebbd6f336928515
Max <maxim_m@todor3d.com>
2022-08-19 01:31:59 +0300

    gui, stats

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import Stats from "three/examples/jsm/libs/stats.module";
+import * as dat from "dat.gui";
+
+const stats = Stats();
+document.body.appendChild(stats.dom);
+
+const gui = new dat.GUI();
 
 const scene = new THREE.Scene();
 
@@ -15,6 +22,19 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+// hemiLight.position.set(0, 200, 0);
+// scene.add(hemiLight);
+
+// const dirLight = new THREE.DirectionalLight(0xffffff);
+// dirLight.position.set(0, 200, 100);
+// dirLight.castShadow = true;
+// dirLight.shadow.camera.top = 180;
+// dirLight.shadow.camera.bottom = -100;
+// dirLight.shadow.camera.left = -120;
+// dirLight.shadow.camera.right = 120;
+// scene.add(dirLight);
+
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
@@ -25,6 +45,15 @@ const material = new THREE.MeshBasicMaterial({
 
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
+const cubeFolder = gui.addFolder("Cube");
+cubeFolder.add(cube.rotation, "x", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "y", 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, "z", 0, Math.PI * 2);
+cubeFolder.open();
+const cameraFolder = gui.addFolder("Camera");
+cameraFolder.add(camera.position, "z", 0, 10);
+cameraFolder.open();
 
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
@@ -37,10 +66,12 @@ function onWindowResize() {
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x += 0.01;
+  // cube.rotation.y += 0.01;
 
   controls.update();
+
+  stats.update();
 
   render();
 }
